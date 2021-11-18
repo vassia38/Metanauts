603820f465177164dd4da96ad4b2ddd1929c3947
vassia38 <alex.19vasile@gmail.com>
2021-11-18 18:08:26 +0200

    added docs for Controller, cleaning

--- src/com/main/controller/ControllerClass.java
+++ src/com/main/controller/ControllerClass.java
@@ -8,7 +8,6 @@ import com.main.service.FriendshipService;
 import com.main.service.UserService;
 
 import java.util.ArrayList;
-import java.util.InputMismatchException;
 import java.util.Objects;
 
 public class ControllerClass implements Controller{
@@ -21,6 +20,11 @@ public class ControllerClass implements Controller{
         this.friendshipService = friendshipService;
     }
 
+    /**
+     * add User(username,firstname,lastname)
+     * @param entity user
+     * @throws RepositoryException if user with same (id and ) username already exists
+     */
     @Override
     public void addUser(User entity) {
         User user = userService.add(entity);
@@ -28,6 +32,12 @@ public class ControllerClass implements Controller{
             throw new RepositoryException("User already exists!\n");
     }
 
+    /**
+     * delete logged-in user and its friendships
+     * @param entity user
+     * @return old state of entity
+     * @throws RepositoryException if user was not found
+     */
     @Override
     public User deleteUser(User entity) {
         User deleted = userService.delete(entity);
@@ -46,6 +56,14 @@ public class ControllerClass implements Controller{
         return deleted;
     }
 
+    /**
+     * update logged-in user
+     * @param entity user
+     * @param firstName new first name
+     * @param lastName new last name
+     * @return old state of entity
+     * @throws RepositoryException if user was not found
+     */
     @Override
     public User updateUser(User entity, String firstName, String lastName){
         entity.setFirstName(firstName);
@@ -56,6 +74,12 @@ public class ControllerClass implements Controller{
         return oldState;
     }
 
+    /**
+     * find user by given id
+     * @param id Long
+     * @return user if found
+     * @throws RepositoryException if user was not found
+     */
     @Override
     public User findUserById(Long id) {
         User user = userService.findOneById(id);
@@ -64,6 +88,12 @@ public class ControllerClass implements Controller{
         return user;
     }
 
+    /**
+     * find user by given username
+     * @param username String
+     * @return user if found
+     * @throws RepositoryException if user was not found
+     */
     @Override
     public User findUserByUsername(String username) {
         User user = userService.findOneByUsername(username);
@@ -72,11 +102,22 @@ public class ControllerClass implements Controller{
         return user;
     }
 
+    /**
+     * get iterable list of all existing users
+     * @return iterable of users
+     */
     @Override
     public Iterable<User> getAllUsers() {
         return userService.getAllEntities();
     }
 
+    /**
+     * add a friendship
+     * @param entity friendship composed of 2 user IDs
+     * @throws RepositoryException if either ID doesn't match an user
+     * @throws RepositoryException if friendship between the 2 users already
+     * exists
+     */
     @Override
     public void addFriendship(Friendship entity) {
         Long id1 = entity.getId().getLeft();
@@ -90,6 +131,14 @@ public class ControllerClass implements Controller{
             throw new RepositoryException("Friendship already exists!\n");
     }
 
+    /**
+     * delete friendship between 2 users, by given IDs
+     * @param entity friendship composed of 2 user IDs
+     * @return old state of friendship
+     * @throws RepositoryException if either ID doesn't match an user
+     * @throws RepositoryException if friendship between the 2 users doesn't
+     * exist
+     */
     @Override
     public Friendship deleteFriendship(Friendship entity) {
         Long id1 = entity.getId().getLeft();
@@ -104,6 +153,13 @@ public class ControllerClass implements Controller{
         return fr;
     }
 
+    /**
+     * update state of friendship between 2 users, by given IDs
+     * @param entity friendship composed of 2 IDs
+     * @return old state of friendship
+     * @throws RepositoryException if friendship between the 2 users doesn't
+     * exist
+     */
     @Override
     public Friendship updateFriendship(Friendship entity){
         Friendship oldState = friendshipService.update(entity);
@@ -112,19 +168,38 @@ public class ControllerClass implements Controller{
         return oldState;
     }
 
+    /**
+     *  get an iterable list of all friendships existing
+     * @return iterable of friendships
+     */
     @Override
     public Iterable<Friendship> getAllFriendships() {
         return friendshipService.getAllEntities();
     }
+
+    /**
+     * draw graph of all existing friendships
+     */
     private void runGraph(){
         graph = new Graph(this);
         graph.runConnectedComponents();
     }
+
+    /**
+     * get list of all communities existing. community = connected component
+     * of users
+     * @return list of IDs list
+     */
     @Override
     public ArrayList<ArrayList<Long>> getAllCommunities(){
         this.runGraph();
         return graph.getCommunities();
     }
+
+    /**
+     * get size of biggest existing community
+     * @return size
+     */
     @Override
     public int getBiggestCommunitySize(){
         this.runGraph();
